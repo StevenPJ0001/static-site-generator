5b479c66c6eb64fbe4b1971660061841410fb4f1
ragdollomega <nonposer@gmail.com>
2026-06-18 20:11:13 -0700

    now hosted on github

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/https://github.com/StevenPJ0001/static-site-generator/"
--- src/gencontent.py
+++ src/gencontent.py
@@ -1,8 +1,24 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_pages_recursive(
+    dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str
+) -> None:
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(
+    from_path: str, template_path: str, dest_path: str | Path, basepath: str
+) -> None:
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -18,6 +34,8 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -26,19 +44,10 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     to_file.write(template)
 
 
+
 def extract_title(md: str) -> str:
     lines = md.split("\n")
     for line in lines:
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
-
-def generate_page_recursive(content_dir, public_dir, template):
-    for item in os.listdir(content_dir):
-        content_path = os.path.join(content_dir, item)
-        public_path = os.path.join(public_dir, item)
-        if os.path.isfile(content_path):
-            output_path = public_path.replace(".md", ".html")
-            generate_page(content_path, template, output_path)
-        else:
-            generate_page_recursive(content_path, public_path, template)
